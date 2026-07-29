471fc273b4baa818bd455d34b8a993cb3b806f95
briyitmu0-wq <briyitmu0@gmail.com>
2026-07-29 15:42:04 -0500

    feat: paneles ciudadano:vacunas pendientes,vacunas aplicadas

--- frontend/sistema_de_vacunacion/src/app/feature/ciudadano/ciudadano-layout/ciudadano-layout.ts
+++ frontend/sistema_de_vacunacion/src/app/feature/ciudadano/ciudadano-layout/ciudadano-layout.ts
@@ -1,9 +1,30 @@
+// feature/ciudadano/ciudadano-layout/ciudadano-layout.ts
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { AppHeader } from '../../../shared/components/app-header/app-header';
+import { AppSidebar, MenuItem } from '../../../shared/components/app-sidebar/app-sidebar';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-ciudadano-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, AppHeader, AppSidebar],
   templateUrl: './ciudadano-layout.html',
-  styleUrl: './ciudadano-layout.css',
+  styleUrl: './ciudadano-layout.css'
 })
-export class CiudadanoLayout {}
+export class CiudadanoLayout {
+
+  nombreUsuario: string;
+
+menuItems: MenuItem[] = [
+  { label: 'Mi carné', ruta: 'carnet', icon: 'bi bi-person' },
+  { label: 'Vacunas aplicadas', ruta: 'vacunas-aplicadas', icon: 'bi bi-clipboard-check' },
+  { label: 'Vacunas pendientes', ruta: 'vacunas-pendientes', icon: 'bi bi-file-medical' },
+  { label: 'Recordatorios', ruta: 'recordatorios', icon: 'bi bi-bell' }
+];
+
+  constructor(private authService: AuthService) {
+   this.nombreUsuario = 'María Ortíz';
+  }
+}
