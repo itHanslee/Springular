955e54632959101b797de6a3a7fa2c33607da92b
yoissy <yoissyaltamar391@gmail.com>
2026-08-14 18:31:41 -0500

    Se cambio vacuna.ts para hacer conexion con el backend

--- sistema_de_vacunacion/src/app/core/services/vacuna.ts
+++ sistema_de_vacunacion/src/app/core/services/vacuna.ts
@@ -1,27 +1,58 @@
-// core/services/administrador/vacuna.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Vacuna } from '../../../app/shared/models/vacuna.model';
 
-@Injectable({ providedIn: 'root' })
+import { API } from '../../../environments/environment';
+import { Vacuna } from '../../shared/models/vacuna.model';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class VacunaService {
-  private readonly baseUrl = '';
 
   constructor(private http: HttpClient) {}
 
   listar(): Observable<Vacuna[]> {
-    return this.http.get<Vacuna[]>(this.baseUrl);
+    return this.http.get<Vacuna[]>(
+      API.VACUNAS.BASE
+    );
   }
+
   listarVacunas(): Observable<Vacuna[]> {
-    return this.http.get<Vacuna[]>(this.baseUrl);
+    return this.http.get<Vacuna[]>(
+      API.VACUNAS.BASE
+    );
   }
 
-  registrar(vacuna: Partial<Vacuna>): Observable<Vacuna> {
-    return this.http.post<Vacuna>(this.baseUrl, vacuna);
+  registrar(
+    vacuna: Partial<Vacuna>
+  ): Observable<Vacuna> {
+    return this.http.post<Vacuna>(
+      API.VACUNAS.BASE,
+      vacuna
+    );
   }
 
-  actualizar(id: number, cambios: Partial<Vacuna>): Observable<Vacuna> {
-    return this.http.put<Vacuna>(`${this.baseUrl}/${id}`, cambios);
+  actualizar(
+    id: number,
+    cambios: Partial<Vacuna>
+  ): Observable<Vacuna> {
+    return this.http.put<Vacuna>(
+      API.VACUNAS.POR_ID(id),
+      cambios
+    );
+  }
+
+  cambiarEstado(
+    id: number,
+    estado: string
+  ): Observable<void> {
+    return this.http.patch<void>(
+      API.VACUNAS.ESTADO(id),
+      null,
+      {
+        params: { estado }
+      }
+    );
   }
 }
