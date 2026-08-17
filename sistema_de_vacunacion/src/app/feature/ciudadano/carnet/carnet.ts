b91d36b1f8f7dc0df8f1ff686f482ab291a6543b
yoissy <yoissyaltamar391@gmail.com>
2026-08-17 15:27:49 -0500

    Se habilito la descarga de carnet para los ciudadanos

--- sistema_de_vacunacion/src/app/feature/ciudadano/carnet/carnet.ts
+++ sistema_de_vacunacion/src/app/feature/ciudadano/carnet/carnet.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth';
+import { CiudadanoService } from '../../../core/services/ciudadano';
 import { Usuario } from '../../../shared/models/usuario.model';
 
 @Component({
@@ -14,8 +15,12 @@ export class Carnet implements OnInit {
   ciudadano = signal<Usuario | null>(null);
 
   cargando = signal(false);
+  descargando = signal(false);
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private ciudadanoService: CiudadanoService
+  ) {}
 
   ngOnInit(): void {
     this.cargarCarnet();
@@ -39,5 +44,54 @@ export class Carnet implements OnInit {
         this.cargando.set(false);
       }
     });
+  }
+
+    descargarCarnet(): void {
+
+    const datos = this.ciudadano();
+
+    if (!datos) {
+      console.error(
+        'No hay información del ciudadano.'
+      );
+
+      return;
+    }
+
+    this.descargando.set(true);
+
+    this.ciudadanoService
+      .descargarCarne(datos.id)
+      .subscribe({
+
+        next: (archivo: Blob) => {
+
+          const url =
+            window.URL.createObjectURL(archivo);
+
+          const enlace =
+            document.createElement('a');
+
+          enlace.href = url;
+
+          enlace.download =
+            'carnet-vacunacion-delta.pdf';
+          enlace.click();
+
+          window.URL.revokeObjectURL(url);
+
+          this.descargando.set(false);
+        },
+
+        error: (error: unknown) => {
+
+          console.error(
+            'Error al descargar el carnet:',
+            error
+          );
+
+          this.descargando.set(false);
+        }
+      });
   }
 }
