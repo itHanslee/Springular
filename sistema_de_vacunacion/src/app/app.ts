341d4221a14e9d40cfd10ffd2dc73175edc35139
Marcos <marcosbohorquez0910@gmail.com>
2026-08-21 09:34:38 -0500

    se arreglo el nombre de los packagejason

--- sistema_de_vacunacion/src/app/app.ts
+++ sistema_de_vacunacion/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('sistema_de_vacuancion');
+  protected readonly title = signal('sistema_de_vacunacion');
 }
