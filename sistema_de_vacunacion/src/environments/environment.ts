538d3373f2f2553f7fa526d2f8d1c425b10c6a08
Marcos <marcosbohorquez0910@gmail.com>
2026-08-21 08:49:39 -0500

    se creo el vercel.json para hacerle deploy

--- sistema_de_vacunacion/src/environments/environment.ts
+++ sistema_de_vacunacion/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = { production: false, apiUrl: 'http://localhost:8080' };
+export const API_BASE_URL = { production: false, apiUrl: 'https://backspring-zmcco.onrender.com' };
 
 export const API = {
 
